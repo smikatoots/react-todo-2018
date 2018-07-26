c643e5f837a730ed9d81836b05d125d1d01ae6b3
Mika Reyes <reyes.mikaelahelene@gmail.com>
2018-07-26 15:59:17 -0700

    tried to setup data base but problem with npm server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
+mongoose.connect(process.env.MONGODB_URI || uri);
 // This line makes the build folder publicly available.
 app.use(express.static('build'));
-
+app.use(bodyParser.json());
 app.use('/db', dbRoutes);
 
 app.listen(3000, () => {
